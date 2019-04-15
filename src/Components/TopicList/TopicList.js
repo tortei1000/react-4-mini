e9866dc7795fc8c5980ede539e28bad15fc70ccc
tiago maffei <tiagomaffei@gmail.com>
2019-04-15 15:00:43 -0600

    done

--- src/Components/TopicList/TopicList.js
+++ src/Components/TopicList/TopicList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import posts from "./../../post_data.json";
 import "./TopicList.css";
+import {Link} from "react-router-dom"
 
 class TopicList extends Component {
   constructor(props) {
@@ -19,7 +20,9 @@ class TopicList extends Component {
   render() {
     const { posts } = this.state;
     let displayTopics = posts.map(post => {
-      return <li key={post.id}>{post.title}</li>;
+      return <li key={post.id}>
+      <link to={`/post/${post.id}`}>{post.title}</link>
+      </li>;
     });
     return (
       <div className="TopicList">
